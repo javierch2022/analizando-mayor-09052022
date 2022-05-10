b6ca68f5908c1456ccbcb57e266ecae1bab47d18
Javier Chavarria <103612867+javierch2022@users.noreply.github.com>
2022-05-09 22:05:01 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+
+
+let numInput:number = Number(prompt("ingrese numero"));
+let mayor = numInput;
+let menor = numInput;
+
+while (numInput != 0){
+  numInput = Number(prompt("ingrese numero"));
+  if (mayor = numInput)
+  console.log ("el numero mayor es:", numInput);
+  else (menor = numInput)
+  console.log ("el numero menor es:", numInput);
+}
+}
